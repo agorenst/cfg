214932491ac8789e66f780c68a08d00542f22426
agorenst <agorenst@cs.wisc.edu>
2015-08-16 22:13:23 -0700

    Neat little CFG class for starter blog post

--- cfg.cpp
+++ cfg.cpp
@@ -11,17 +11,16 @@
 
 using namespace std;
 
-
-
 // This is a non-exposed helper function.
 template<class S>
-std::vector<S> tokenize(S s) {
-    std::stringstream strstr(s);
+vector<S> tokenize(S s) {
+    stringstream strstr(s);
     // use stream iterators to copy the stream to the vector as whitespace separated strings
-    std::istream_iterator<S> it(strstr);
-    return std::vector<S>(it, std::istream_iterator<S>());
+    istream_iterator<S> it(strstr);
+    return vector<S>(it, istream_iterator<S>());
 }
 
+// Here are the implementations of the exposed methods.
 namespace cfg {
     production grammar::operator[](const int i) const {
         auto finder = prods.begin();
@@ -37,6 +36,9 @@ namespace cfg {
         return -1;
     }
 
+    // This exploits the fact that productions are ordered.
+    // The start symbol is always just the first nonterminal
+    // we see.
     symbol grammar::start_symbol() const {
         return prods.begin()->lhs;
     }
@@ -46,56 +48,80 @@ namespace cfg {
         assert(is_nonterminal(lhs));
         sequence<production> ret_val = {};
 
-        std::copy_if(prods.begin(), prods.end(), std::back_inserter(ret_val),
+        copy_if(prods.begin(), prods.end(), back_inserter(ret_val),
                 [&](const production& p) { return p.lhs == lhs; });
 
         return ret_val;
     }
 
     bool grammar::is_nonterminal(const symbol& s) const {
-        return std::any_of(prods.begin(), prods.end(),
+        return any_of(prods.begin(), prods.end(),
                 [&](const production &p) { return p.lhs == s; });
     }
     bool grammar::is_terminal(const symbol& s) const {
         return !is_nonterminal(s);
     }
 
-    // IO operations
-    grammar read_grammar(std::istream& input) {
+    // IO operations.
+
+    // This essentially defines the format we expect the CFG
+    // to be in. It's pretty simple, and counts on everything
+    // being whitespace delimited.
+    grammar read_grammar(istream& input) {
         sequence<production> production_list = {};
 
-        std::string line;
+        string line;
         while(getline(input, line)) {
-            std::vector<symbol> tokens = tokenize(line);
+            vector<symbol> tokens = tokenize(line);
             if (tokens.size() == 0) { continue; }
-            production new_prod{*tokens.begin(), sequence<symbol>{next(tokens.begin()), tokens.end()}};
+            production new_prod{
+                *tokens.begin(), // lhs
+                sequence<symbol>{next(tokens.begin()), tokens.end()} // rhs
+            };
             production_list.push_back(new_prod);
         }
         return grammar{production_list};
     }
 
-    std::ostream& operator<<(std::ostream& o, const production& p) {
+    // Because we insert whitespace here, we should be able to read
+    // back in any CFG we print out.
+    ostream& operator<<(ostream& o, const production& p) {
         o << p.lhs << " ";
         for (auto&& s : p.rhs) {
             o << s << " ";
         }
         return o;
     }
-
-    std::ostream& operator<<(std::ostream& o, const grammar& g) {
+    ostream& operator<<(ostream& o, const grammar& g) {
         for (auto&& p : g.prods) {
-            o << p << std::endl;
+            o << p << endl;
         }
-        o << std::endl;
+        o << endl;
         return o;
     }
 
 }
 
+// What follows here is user code, exercising just a small bit
+// of the CFG class to make sure nothing's absolutely egregiously
+// wrong.
+
 using namespace cfg;
 
+// An example of how we can succinctly define grammars "in-code"
+// for the hard-coded case.
+const grammar arithmetic{
+    {"S", "S", "+", "S"},
+    {"S", "S", "-", "S"},
+    {"S", "S", "/", "S"},
+    {"S", "S", "*", "S"},
+    {"S", "n"}
+};
+
 int main() {
-    grammar g = read_grammar(std::cin);
-    std::cout << g << std::endl;
+    // Sample usage: ./cfg < arith.cfg
+    grammar g = read_grammar(cin);
+    cout << g << endl;
+    cout << arithmetic << endl;
     return 0;
 }
