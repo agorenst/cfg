e8115864bdd0e943f109ce3ebc89935b5e40db67
agorenst <agorenst@cs.wisc.edu>
2015-08-16 20:34:02 -0700

    Basic reading and writing of text representations of cfgs

--- cfg.h
+++ cfg.h
@@ -1,59 +1,78 @@
 #include <string>
+#include <list>
+#include <memory>
+#include <algorithm>
+#include <cassert>
 
-typedef std::string symbol
-template<typename T> std::list<T> sequence
+typedef std::string symbol;
+
+template <typename T> using sequence = std::list<T>;
+
+typedef sequence<symbol>::const_iterator seq_iter;
 
 class production {
-private:
-symbol LHS
-sequence<symbol> RHS
-public:
+    public:
+        const symbol lhs;
+        const sequence<symbol> rhs;
+        bool operator==(const production& p) const {
+            return lhs == p.lhs && rhs == p.rhs;
+        }
 };
+
+class grammar;
+
+class parse_tree;
+typedef std::shared_ptr<parse_tree> ptree;
+
 
 class grammar {
-private:
-  sequence<production> productions
-public:
-  // Map integers to productions
-  production operator[](const int ix) const {
-    auto iter = productions.begin();
-    for (int i = 0; i < ix; ++i, ++iter);
-    return *iter;
-  }
+    public:
+        const sequence<production> productions;
 
-  // Given a symbol, return all productions that have that as their LHS
-  // Really a filter over all productions
-  sequence<production> productions_from_nonterminal(const symbol lhs) const {
-    sequence<production> ret_value = {};
-    for (auto&& p : productions) {
-      if (p.lhs == lhs) {
-        ret_value.insert(p);
-      }
-    }
-    return ret_value;
-  }
+    public:
+        grammar(sequence<production> prods) : productions(prods) {}
+        // Map integers to productions
+        production operator[](const int ix) const {
+            auto iter = productions.begin();
+            for (int i = 0; i < ix; ++i, ++iter)
+                ;
+            return *iter;
+        }
 
-  // Given a sequence of symbols and an iterator that promises to point to a nonterminal in that sequence,
-  // create a new sequence for each possible production implied by that nonterminal.
-  // TODO: make this create a parse tree instead? Make it all more implied, you know.
-  sequence<sequence<symbol>> develop_seq_at_iter(const sequence<symbol>& seq, sequence<symbol>::const_iterator splitter) const {
-    assert(is_nonterminal(*splitter));
-    // assert that splitter is an iterator in the collection "seq"
-    sequence<sequence<symbol>> ret_value = {}
-    for (auto&& prod : production_from_nonterminal(*splitter)) {
-      sequence<symbol> next_development = {}
-      next_development.insert(next_development.end(), seq.begin(), splitter);
-      next_development.insert(next_development.end(), prod.rhs.begin(), prod.rhs.end());
-      next_development.insert(next_development.end(), splitter+1, seq.end());
-      ret_value.push_back(next_development);
-    }
-    return ret_value;
-  }
+        int index_of(const production p) const {
+            int i = 0;
+            for (auto &&q : productions) {
+                if (q == p) {
+                    return i;
+                }
+                ++i;
+            }
+            return i;
+        }
+
+        // Given a symbol, return all productions that have that as their LHS
+        // Really a filter over all productions
+        sequence<production> productions_from_nonterminal(const symbol lhs) const {
+            sequence<production> ret_value = {};
+            for (auto &&p : productions) {
+                if (p.lhs == lhs) {
+                    ret_value.insert(ret_value.begin(), p);
+                }
+            }
+            return ret_value;
+        }
+
+        symbol start_symbol() const { return productions.begin()->lhs; }
+
+        // Given a sequence of symbols and an iterator that promises to point to a
+        // nonterminal in that sequence,
+        // create a new sequence for each possible production implied by that
+        // nonterminal.
+        // TODO: make this create a parse tree instead? Make it all more implied, you
+        // know.
+        sequence<sequence<symbol>>
+            develop_seq_at_iter(const sequence<symbol> &seq,
+                    sequence<symbol>::const_iterator splitter) const;
+
+        bool is_nonterminal(const symbol &s) const;
 };
-
-class parse_tree {
-public:
-  const int production_id;
-  const grammar& grammar;
-  const sequence<std::shared_ptr<parse_tree>> children;
-}
